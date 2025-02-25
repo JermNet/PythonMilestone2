1544012cc87528ee68710fb55b5a7e3221ca6a44
Jeremy <jeremywhitenect@gmail.com>
2025-02-25 11:18:56 -0400

    Cleans up and comments most of the code

--- tempCodeRunnerFile.py
+++
@@ -1,7 +0,0 @@
- # if self.shop_system.selected_snake_skin:
-        #     self.selected_snake_skin = self.shop_system.selected_snake_skin
-        #     self.snake_skin = self.selected_snake_skin
-        #     print("yep")
-        # if self.shop_system.selected_food_skin:
-        #     self.selected_food_skin = self.shop_system.selected_food_skin
-        #     self.food_skin = self.selected_food_skin
